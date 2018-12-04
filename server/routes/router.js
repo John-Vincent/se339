7d5cb950b02ac8495b26f000a276bb4a7a7d86b2
John-Vincent <collinvincent96@gmail.com>
2018-12-03 23:05:04 -0600

    maybe fixed routers

--- server/routes/router.js
+++ server/routes/router.js
@@ -6,19 +6,20 @@ var example = require(path.resolve(__dirname, '../controllers/exampleController'
 var manager = require(path.resolve(__dirname, '../controllers/managerController'));
 var vehicle = require(path.resolve(__dirname, '../controllers/vehicleController'));
 
-auth.use(authguard);
-
 //Example of how to render html file upon get request
 auth.route('/example')
+    .all(authguard)
     .get(example.listAll)
     .post(example.create);
 
 auth.route('/example/:vid')
+    .all(authguard)
     .get(example.getByUid)
     .put(example.updateExample)
     .delete(example.deleteByUid);
 
 auth.route('/manager')
+    .all(authguard)
     .get(manager.listAll);
 
 noauth.route('/manager')
@@ -28,39 +29,49 @@ noauth.route('/manager/login')
     .post(manager.comparePassword)
 
 auth.route('/manager/:username')
+    .all(authguard)
     .get(manager.getVehicles)
     .put(manager.updateVehicles)
     .delete(manager.deleteVehicle);
 
 auth.route('/vehicle')
+    .all(authguard)
     .get(vehicle.listAll)
     .post(vehicle.create);
 
 auth.route('/vehicle/init/:vid')
+    .all(authguard)
     .get(vehicle.init);
 
 auth.route('/vehicle/:vid')
+    .all(authguard)
     .get(vehicle.getByUid)
     .put(vehicle.updateVehicle)
     .delete(vehicle.deleteByUid);
 
 auth.route('/vehicle/message/:vid')
+    .all(authguard)
     .put(vehicle.updateMessage);
 
 auth.route('/vehicle/send/:vid')
+    .all(authguard)
     .put(vehicle.updatePid);
 
 auth.route('/vehicle/send/:vid/:pid')
+    .all(authguard)
     .delete(vehicle.deletePid);
 
 auth.route('/vehicle/pid/:vid')
+    .all(authguard)
     .put(vehicle.updateVehiclePid);
 
 auth.route('/manager/chart/:user')
+    .all(authguard)
     .get(manager.getChart)
     .put(manager.updateChart);
 
 auth.route('/manager/chart/update/:user')
+    .all(authguard)
     .put(manager.editChart);
 
 module.exports = {auth: auth, noauth: noauth};
