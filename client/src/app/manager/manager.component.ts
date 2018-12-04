b7db16551ef18a051b9413261a85e0c33199024b
mattb <mattbechtel123@gmail.com>
2018-12-04 13:46:17 -0600

    Added a view to the Manager dashboard. Also added code to the component to pull in all the vehicles when the user loads the page, so that the manager can add vehicles to their 'MyVehicles' list

--- client/src/app/manager/manager.component.ts
+++ client/src/app/manager/manager.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ManagerService } from './manager.service'
 import { AuthService } from '../services/auth.service';
+import { ApiService } from '../services/api.service';
 import { Manager } from '../models/manager.model';
+import { Vehicle } from '../models/vehicle.model';
+
 
 @Component(
     {
@@ -14,15 +17,43 @@ import { Manager } from '../models/manager.model';
 export class ManagerComponent implements OnInit 
 {
     private manager: Manager;
+    private addingVehicle: boolean;
+    private creatingVehicle: boolean;
+    private allVehicles: Array<Vehicle>
 
     constructor(
         private authService: AuthService,
+        private apiService: ApiService,
         private router: Router 
     ) { }
 
     ngOnInit() 
     {
+        this.addingVehicle = false;
+        this.creatingVehicle = false;
         this.manager = this.authService.getManager();
+        var that = this;
+        this.apiService.getVehicles().then(function(data){
+            that.allVehicles = data;
+        }).catch((err) => 
+        {
+            console.log("Error loading vehicles", err);
+        });
         console.log(this.manager); 
+    }
+
+    addVehicleView()
+    {
+        this.addingVehicle = true;
+    }
+
+    addVehicle(vehicle: Vehicle)
+    {
+        console.log("Adding Vehicle: " + vehicle);
+    }
+
+    createVehicleView()
+    {
+        this.creatingVehicle = true;
     }
 }
